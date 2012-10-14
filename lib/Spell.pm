4a72ccedd323904d18155e525a4465b28a50fe53
Gabriel M Deal <gabrielx@yellowleaf.org>
2012-10-14 12:26:49 -0700

    Adding a few files I missed in the initial commit.

---
+++ lib/Spell.pm
@@ -0,0 +1,101 @@
+package Spell;
+
+use strict;
+
+my %gWords;
+
+sub initialize {
+    my ($dir) = @_;
+
+    my @files = glob "$dir/*" or die "No dictionaries in '$dir'";
+    for my $file (@files) {
+        open(IN,$file) or die "Could not open dictionary '$file': $!\n";
+        my @words = map { (split(/\s+/, lc($_))) } <IN>;
+        chomp @words;
+        @gWords{@words} = ();
+    }
+}
+
+sub split_words {
+    my ($text) = @_;
+
+    my @words;
+    foreach my $word (split /[\s-\/]+/, $text) {
+        $word =~ s/'s$//;
+        push @words, $word;
+    }
+
+    return @words;
+}
+
+sub add_words {
+    my ($text) = @_;
+
+    foreach my $word (split_words($text)) {
+        $gWords{strip_word(lc($word))} = 1;
+    }
+}
+
+sub strip_word {
+    my ($word) = @_;
+
+    # remove climb ratings like 5.10d:
+    $word =~ s/\b(5\.)?1[0-5][abcd]\b//;
+
+    $word =~ s/\b(1st|2nd|3rd|[4-9]th|\d+th)\b//;
+
+    $word =~ s/^[\W_]+//;
+    $word =~ s/[\W\d_]+$//;
+
+    return $word;
+
+}
+
+sub possibilities {
+    my ($word) = @_;
+
+    my @regexes = ('(.*)s',
+                   '(.*s)es',
+                   '(.*)ing',
+                   '(.*)ed',
+                   '(.*)able',
+                   '(.*)ing',
+                   'un(.*)',
+                  );
+
+    my @possibilites;
+    foreach my $regex (@regexes) {
+        if ($word =~ /^$regex$/) {
+            push @possibilites, $1;
+        }
+    }
+
+    my $word1 = $word;
+    if ($word1 =~ s/ies$/y/i) {
+        push @possibilites, $word1;
+    }
+
+    return ($word, @possibilites);
+}
+
+sub check {
+    my ($text) = @_;
+
+    $text =~ s,\bhttp://\S+,,g;
+
+    chomp $text;
+    $text = lc $text;
+
+    my @misspelled;
+    for my $word (grep {/[^\W\d_]/} split_words($text)) {
+        $word = strip_word($word);
+
+        if (! grep { exists $gWords{$_} } possibilities($word)) {
+            push @misspelled, $word;
+        }
+    }
+
+    return sort @misspelled;
+}
+
+1;
