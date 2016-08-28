28aafe761b85837ca0e1d9cb500635968e0ea224
Gabriel M Deal <gabriel@dicksonlabs.com>
2016-08-27 20:23:30 -0700

    De-legacyize some paths
    data/locations/converted -> data/locations
    data/gabrielx/images -> data/gabrielx/reports

--- lib/Scramble/Location.pm
+++ lib/Scramble/Location.pm
@@ -361,7 +361,7 @@ sub set_have_visited {
 ######################################################################
 
 sub open_all {
-    open_specific(sort glob("data/locations/converted/*.xml"));
+    open_specific(sort glob("data/locations/*.xml"));
 }
 sub open_specific {
     my (@paths) = @_;
@@ -451,7 +451,7 @@ sub find_location {
 
     # Lazily load the location:
     my $name = $args{'name'} || die "Not given name";
-    my $path = sprintf("data/locations/converted/%s.xml", Scramble::Misc::sanitize_for_filename($name));
+    my $path = sprintf("data/locations/%s.xml", Scramble::Misc::sanitize_for_filename($name));
     die "Unable to lazily load from $path" unless -f $path;
     open_specific($path);
 
