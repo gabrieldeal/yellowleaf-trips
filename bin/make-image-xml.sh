87e8b0beff769ec41939f0ca1892a79180f5ce47
Gabriel M Deal <gabriel@dicksonlabs.com>
2018-12-17 04:15:18 +0000

    Delete unused bin/* programs

--- bin/make-image-xml.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-#c:/Perl64/bin/perl c:/cygwin/home/gabrielx/scramble/bin/make-image-xml
-c:/Perl64/bin/perl c:/cygwin/home/gabrielx/scramble/bin/make-image-xml "$@"
--- bin/make-report.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-umask 0022;cd ~/projects/yellowleaf-trips && perl bin/make-report "$@" fuck-line-feeds
