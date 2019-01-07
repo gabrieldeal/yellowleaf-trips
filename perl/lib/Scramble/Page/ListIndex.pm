a16844e09c3ad88e0452b5d46c21aa6c476f6cbd
Gabriel M Deal <gabriel@dicksonlabs.com>
2019-01-07 15:42:42 -0800

    Convert list index to template.

--- perl/lib/Scramble/Page/ListIndex.pm
+++ perl/lib/Scramble/Page/ListIndex.pm
@@ -8,31 +8,25 @@ use Scramble::Misc ();
 # FIXME: Refactor display code into a template.
 
 sub create {
-    my $index_html;
+    my @lists;
     foreach my $list_xml (Scramble::Model::List::get_all_lists()) {
         if ($list_xml->{'skip'}) {
             next;
         }
 
-        # FIXME: The if & else seem nearly identical...
-        if (! $list_xml->{'no-display'}) {
-            $index_html .= sprintf(qq(<li>%s</li>), Scramble::Model::List::make_list_link($list_xml));
-	} elsif ($list_xml->{'URL'}) {
-            $index_html .= sprintf(qq(<li><a href="%s">%s</li>),
-                                   $list_xml->{URL},
-                                   $list_xml->{name});
-        }
+        push @lists, {
+            name => $list_xml->{name},
+            url => $list_xml->{URL}
+        };
     }
 
-    $index_html = <<EOT;
-<ul>
-$index_html
-</ul>
-EOT
+    my $template = Scramble::Template::create('list/index');
+    $template->param(lists => \@lists);
+    my $html = $template->output();
 
     Scramble::Misc::create("li/index.html",
                            Scramble::Misc::make_1_column_page(title => "Peak Lists",
-                                                              html => $index_html,
+                                                              html => $html,
                                                               'include-header' => 1));
 }
 
